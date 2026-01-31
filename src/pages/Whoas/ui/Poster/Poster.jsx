85ca11bb83fa714d23599be70ca8f95c797b96ca
perfilev-danil <danil.perfiliv@gmail.com>
2026-01-31 09:14:02 +0300

    poster loading

--- src/pages/Whoas/ui/Poster/Poster.jsx
+++ src/pages/Whoas/ui/Poster/Poster.jsx
@@ -1,10 +1,22 @@
 import "./Poster.css";
+import { useState } from "react";
+import Loader from "../../../../shared/ui/Loader/Loader";
 
 const Poster = ({ movie }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
     <article className="poster">
-      <div className="poster__image-wrapper">
-        <img className="poster__image" src={movie.poster} alt={movie.movie} />
+      <div className="poster__img-wrapper">
+        {!isLoaded && <Loader />}
+
+        <img
+          className={`poster__img ${isLoaded ? "poster__img--visible" : ""}`}
+          src={movie.poster}
+          alt={movie.movie}
+          loading="lazy"
+          onLoad={() => setIsLoaded(true)}
+        />
       </div>
     </article>
   );
