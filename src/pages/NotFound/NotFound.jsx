08b28d7efe8ca0cddbd1abcd8035e9fab4d73b37
perfilev-danil <danil.perfiliv@gmail.com>
2026-01-31 09:42:48 +0300

    fixes: NotFound page styles

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,16 @@
+import { routes } from "../../app/router/routes";
+import "./NotFound.css";
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="">
-      Seems like your whoas <br /> are not fount :C
-      <Link to="/">Go Home</Link>
+    <div className="not-found centered">
+      <p>
+        Seems like your whoas <br /> are not found :C
+      </p>
+      <Link className="not-found__go-home" to={routes.Home}>
+        Go Home
+      </Link>
     </div>
   );
 };
